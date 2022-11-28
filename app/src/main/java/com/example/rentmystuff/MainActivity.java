e36555c384b0da5272f54d84a8ec6f65b75313f7
Or Yitshak <tur3103@gmail.com>
2022-11-28 15:38:06 +0200

    change layout files

--- app/src/main/java/com/example/rentmystuff/MainActivity.java
+++ app/src/main/java/com/example/rentmystuff/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username_etxt;
+    EditText email_etxt;
     EditText password_etxt;
     Button login_btn;
     Button register_btn;
@@ -20,11 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username_etxt = findViewById(R.id.usernameETxt);
+        email_etxt = findViewById(R.id.emailETxt);
         password_etxt = findViewById(R.id.passwordETxt);
         login_btn = findViewById(R.id.loginBtn);
         register_btn = findViewById(R.id.registerBtn);
-
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/rentmystuff/RegisterActivity.java
+++ app/src/main/java/com/example/rentmystuff/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.example.rentmystuff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText email_etxt;
+    EditText password_etxt;
+    Button login_btn;
+    Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email_etxt = findViewById(R.id.emailETxt);
+        password_etxt = findViewById(R.id.passwordETxt);
+        login_btn = findViewById(R.id.loginBtn);
+        register_btn = findViewById(R.id.registerBtn);
     }
 }
