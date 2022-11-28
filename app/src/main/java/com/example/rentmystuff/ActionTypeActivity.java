64892de8fb97eac0d6a4fb2d17e556a5d69123f8
LeeadJ <leead123@gmail.com>
2022-11-28 16:26:39 +0200

    added actionType activity

---
+++ app/src/main/java/com/example/rentmystuff/ActionTypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.rentmystuff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_type);
+    }
+}
