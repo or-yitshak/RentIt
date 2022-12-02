52fac5165f3bd10a2a97612981a07d3271717abc
C:/Users/yahal <yahalomhasid@gmail.com>
2022-12-02 12:24:38 +0200

    option to upload image to firebase storage

--- app/src/main/java/com/example/rentmystuff/PostActivity.java
+++ app/src/main/java/com/example/rentmystuff/PostActivity.java
@@ -1,14 +1,95 @@
 package com.example.rentmystuff;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
+
+import javax.annotation.Nullable;
 
 public class PostActivity extends AppCompatActivity {
 
+    ImageButton upload_btn;
+    ImageView image_view;
+    Uri imageUri;
+    FirebaseStorage storage;
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        upload_btn = findViewById(R.id.uploadButton);
+        image_view = findViewById(R.id.imageView);
+        storage = FirebaseStorage.getInstance();
+        image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityResultLauncher.launch("Images/*");
+
+            }
+        });
+
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+
     }
+
+    private void uploadImage() {
+
+        if(imageUri != null){
+            StorageReference reference = storage.getReference().child("Images/" + UUID.randomUUID().toString());
+            reference.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful()) {
+                        //image uploaded
+                        Toast.makeText(PostActivity.this, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                    } else{
+                        Toast.makeText(PostActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<String> ActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if(result != null){
+                        image_view.setImageURI(result);
+                        imageUri = result;
+                    }
+                }
+            });
+
+
 }
